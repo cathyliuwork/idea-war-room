918a4239e5c8ad46ec5eba49f017644d48783c2a
Tyler Chao <cz848@qq.com>
2026-01-29 01:20:49 +0800

    refactor: no errors if no test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
     globals: true,
+    passWithNoTests: true,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
